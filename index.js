061f438d5f277862b2b4d8d9ff7d11ab3ac2eb4f
kiwaeva <kiwaeva@gmail.com>
2025-10-07 18:50:19 +0100

    Copy paste the exercises.

---
+++ index.js
@@ -0,0 +1,12 @@
+//Map to names
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let users = [ john, pete, mary ];
+
+let names = /* ... your code */
+
+alert( names ); // John, Pete, Mary
+
+//Map to objects
